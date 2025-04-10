0b4c505cca144db97b425260f4ae10521ebf6f5b
Daniel Neves <danielnevesdecastro@gmail.com>
2025-04-10 20:10:01 -0300

    Feat: JPA Config

--- produtosapi/src/main/java/danielnevesdecastro/com/github/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/danielnevesdecastro/com/github/produtosapi/model/Produto.java
@@ -1,11 +1,27 @@
 package danielnevesdecastro.com.github.produtosapi.model;
 
 //POJO -
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column()
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public String getId() {
